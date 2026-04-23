bc305598a4d38343e66e348ee2f435e2d9ae4592
Boyan G. Kiroff <boyan.kiroff@brandmaker.com>
2026-04-23 12:03:27 +0300

    Add funds API, account closure, and event sourcing improvements
    * Introduced the FundsController for handling deposit and withdrawal operations via HTTP.
    * Refined deposit logic to disallow non-positive amounts.
    * Enhanced event sourcing handlers with detailed Javadoc and timestamp handling.
    * Updated AccountsController to support closing accounts through a new DELETE endpoint.

--- src/main/java/org/kiroff/bank/account/common/events/AccountClosedEvent.java
+++ src/main/java/org/kiroff/bank/account/common/events/AccountClosedEvent.java
@@ -1,12 +1,15 @@
 package org.kiroff.bank.account.common.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.kiroff.bank.cqrs.core.events.BaseEvent;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public class AccountClosedEvent extends BaseEvent {
 
